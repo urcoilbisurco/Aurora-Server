6b95443c05fbb1cef51dc281038bd1ec2f2123c8
Francesco Marassi <urcoilbisurco@gmail.com>
2017-03-25 14:40:26 +0100

    add support for last_day values for temperature

--- utils/mqtt.js
+++ utils/mqtt.js
@@ -20,15 +20,14 @@ function update(topic, message){
   var node=topic.split("/")[1]
   var data=JSON.parse(message)
   //TODO: refactor this part.
-  db.nodes.get(user, node)
-  .then(n){
+  db.nodes.get(user, node).then(function(n){
     if(n.type=="temperature"){
       var last_day_arr=n.state.last_day || []
       last_day_arr.push(data.temp)
       data.last_day=last_day_arr.slice(Math.max(last_day_arr-24, 1))
     }
     db.nodes.updateState(user, node, data);
-  }
+  })
 }
 
 module.exports=client;
