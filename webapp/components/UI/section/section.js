3c1054360bd7ba8103a5aa50365bade3d8096ce6
Francesco Marassi <urcoilbisurco@gmail.com>
2017-04-20 11:13:07 +0200

    updated controls layout to better handle more than 5 nodes

--- webapp/components/UI/section/section.js
+++ webapp/components/UI/section/section.js
@@ -7,9 +7,11 @@ var Section=function(props){
   return (
     <div className={cn(css.section)}>
       <div className={css.section_title}>{props.title}</div>
-      <span className={props.direction=="horizontal" ? css.horizontal : ""}>
-      {props.children}
-      </span>
+      <div className={css.cont}>
+        <span className={props.direction=="horizontal" ? css.horizontal : ""}>
+        {props.children}
+        </span>
+      </div>
     </div>
   )
 };
